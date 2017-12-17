0b349a91de6c67214714dd78c832c64a791ffb78
khgnwebdev <34625506+khgnwebdev@users.noreply.github.com>
2017-12-17 16:33:07 -0500

    Delete hiveconn.php

--- inc/hiveconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$dbname = "khgnwebcom_Cemeteries";
-$username = "khgnwebcom_cemr";
-$password = "ixD&M~8yT?u}}HHN7^3Xh3k";
-$rconn=new PDO("mysql:host=$servername;dbname=$dbname;", $username, $password);
-$wconn=new PDO("mysql:host=$servername;dbname=$dbname;", "khgnwebcom_cemrw", "bT3I9EL.5phDiO36UBy,FKZ1");
-?>
